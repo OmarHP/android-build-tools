d3f15488786f74e0c4d21b549911db14f6523ce4
Omar Ham <omar.hp90@gmail.com>
2024-09-25 12:11:57 -0500

    Add support for BaseAndroidPlugin to apply more standard configurations

--- plugins/src/main/java/com/omarhp/android/build/plugins/BaseAndroidPlugin.kt
+++ plugins/src/main/java/com/omarhp/android/build/plugins/BaseAndroidPlugin.kt
@@ -8,12 +8,12 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinAndroidPluginWrapper
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 abstract class BaseAndroidPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
-
-        // Apply kotlin-android plugin
+        // Apply the Kotlin Android plugin
         project.pluginManager.apply(KotlinAndroidPluginWrapper::class.java)
 
         // Get configuration extensions
@@ -44,9 +44,32 @@ abstract class BaseAndroidPlugin : Plugin<Project> {
             targetCompatibility = JavaVersion.toVersion(BuildConfig.JAVA_VERSION)
         }
 
+        // Set Kotlin options
+        project.tasks.withType(KotlinCompile::class.java).configureEach { task ->
+            task.kotlinOptions {
+                jvmTarget = BuildConfig.JAVA_VERSION.toString()
+            }
+        }
+
+        // Set proguard configuration
+        baseExtension.buildTypes { types ->
+            types.getByName("release") { type ->
+                type.isMinifyEnabled = appExtension != null
+                type.proguardFiles(
+                    baseExtension.getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+                )
+            }
+        }
+
+        libraryExtension?.defaultConfig {
+            consumerProguardFiles("consumer-rules.pro")
+        }
+
         // Set the buildFeatures block
         with(baseExtension.buildFeatures) {
             compose = true
+            buildConfig = true
         }
 
         // Set the compose options
@@ -57,6 +80,13 @@ abstract class BaseAndroidPlugin : Plugin<Project> {
         // Exclude license resources in the '/META-INF/' directory
         baseExtension.packagingOptions.resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "/META-INF/*.kotlin_module"
+            excludes += "/META-INF/{LICENSE,LICENSE.md,LICENSE.txt,LICENSE-notice.md}"
+            excludes += "/META-INF/proguard/androidx-annotations.pro"
+            excludes += "/META-INF/{NOTICE, NOTICE.md, NOTICE.txt}"
+            excludes += "/META-INF/{rxjava.properties}"
+            excludes += "/META-INF/{kotlinx_coroutines_core.version}"
+            excludes += "/META-INF/{kotlinx_coroutines_test.version}"
         }
 
         // Set the JVMToolChain
